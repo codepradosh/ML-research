d5beee3b978744781cf6732cb6817ced07a46f52
Pradosh Priyadarshan <45397701+codepradosh@users.noreply.github.com>
2023-06-19 15:00:08 +0530

    Update dmzdemo.py

--- dmzdemo.py
+++ dmzdemo.py
@@ -5,21 +5,17 @@ import sys
 def check_service_status(service_name, build_number):
     command = get_validation_command(service_name, build_number)
     try:
-        if sys.version_info < (3, 0):
-            process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            stdout, stderr = process.communicate()
-            if process.returncode != 0:
-                raise subprocess.CalledProcessError(process.returncode, command)
-        else:
-            result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-            stdout = result.stdout
-            stderr = result.stderr
-
-        return stdout.decode().strip()
+        result = subprocess.run(command, shell=True, capture_output=True)
+        stdout = result.stdout.decode().strip()
+        stderr = result.stderr.decode().strip()
+        print("stdout:", stdout)
+        print("stderr:", stderr)
+        if result.returncode != 0:
+            raise subprocess.CalledProcessError(result.returncode, command)
+        return stdout
     except subprocess.CalledProcessError as e:
         print("Error occurred while checking service status: {0}".format(e))
         sys.exit(1)
-
 
 # Rest of the code...
 
