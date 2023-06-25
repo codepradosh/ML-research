d105280c0a81d10c15e897fbcc815731a79655a2
Pradosh Priyadarshan <45397701+codepradosh@users.noreply.github.com>
2023-06-25 21:12:38 +0530

    Update LSTM_Coreimplementation.py

--- LSTM_Coreimplementation.py
+++ LSTM_Coreimplementation.py
@@ -4,8 +4,6 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense
-from sklearn.model_selection import GridSearchCV
-from tensorflow.keras.wrappers.scikit_learn import KerasClassifier
 from scipy.stats import genextreme
 
 # Step 1: Load the dataset
@@ -49,52 +47,22 @@ scaled_data = scaler.fit_transform(combined_df.drop(columns=['Time', 'Disk Avera
 X = scaled_data.reshape(scaled_data.shape[0], 1, scaled_data.shape[1])
 y = combined_df[['Disk Average IO Label', 'Label Disk Queue Time', 'Disk Health']].values
 
-# Step 7: Create a function to build the LSTM model
-def create_lstm_model(units=64, dropout=0.2):
-    model = Sequential()
-    model.add(LSTM(units, input_shape=(1, X.shape[2])))
-    model.add(Dense(3, activation='sigmoid'))
-    model.compile(loss='binary_crossentropy', optimizer='adam')
-    return model
+# Step 7: Build LSTM model
+model = Sequential()
+model.add(LSTM(64, input_shape=(1, X.shape[2])))
+model.add(Dense(3, activation='sigmoid'))
+model.compile(loss='binary_crossentropy', optimizer='adam')
 
-# Step 8: Create the KerasClassifier wrapper
-model_wrapper = KerasClassifier(build_fn=create_lstm_model, verbose=0)
+# Step 8: Train LSTM model
+model.fit(X, y, epochs=10, batch_size=16, verbose=1)
 
-# Step 9: Define the hyperparameters to tune
-param_grid = {
-    'units': [32, 64, 128],  # Number of LSTM units
-    'dropout': [0.2, 0.3, 0.4],  # Dropout rate
-    'epochs': [10, 20, 30],  # Number of epochs
-    'batch_size': [16, 32, 64]  # Batch size
-}
+# Step 9: Calculate composite scores for training data
+train_composite_scores = model.predict(X)
 
-# Step 10: Create the GridSearchCV object
-grid_search = GridSearchCV(estimator=model_wrapper, param_grid=param_grid, cv=3)
-
-# Step 11: Perform grid search
-grid_result = grid_search.fit(X, y)
-
-# Step 12: Print the best hyperparameters found
-print("Best Hyperparameters: ", grid_result.best_params_)
-
-# Step 13: Build the final LSTM model with the best hyperparameters
-best_units = grid_result.best_params_['units']
-best_dropout = grid_result.best_params_['dropout']
-best_epochs = grid_result.best_params_['epochs']
-best_batch_size = grid_result.best_params_['batch_size']
-
-final_model = create_lstm_model(units=best_units, dropout=best_dropout)
-
-# Step 14: Train the final LSTM model
-final_model.fit(X, y, epochs=best_epochs, batch_size=best_batch_size, verbose=1)
-
-# Step 15: Calculate composite scores for training data
-train_composite_scores = final_model.predict(X)
-
-# Step 16: Determine the dynamic threshold for the training data using Extreme Value Distribution
+# Step 10: Determine the dynamic threshold for the training data using Extreme Value Distribution
 threshold = genextreme.ppf(0.95, loc=np.mean(train_composite_scores[:, 0]), scale=np.std(train_composite_scores[:, 0]))
 
-# Step 17: Plot the composite function with the dynamic threshold for the training data
+# Step 11: Plot the composite function with the dynamic threshold for the training data
 plt.figure(figsize=(10, 6))
 plt.plot(df['Time'], train_composite_scores[:, 0], color='blue')
 plt.axhline(threshold, color='red', linestyle='--', label='Threshold')
@@ -104,7 +72,7 @@ plt.title('Composite Function Graph')
 plt.legend()
 plt.show()
 
-# Step 18: Determine disk health and busyness levels for the training data
+# Step 12: Determine disk health and busyness levels for the training data
 disk_health = np.where(train_composite_scores[:, 0] <= threshold, 'Healthy', 'Unhealthy')
 busyness_levels = np.where(train_composite_scores[:, 0] <= threshold, 'Low', 'High')
 
