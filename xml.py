dabb6f90b9e7116f5d9ef2e55bbeeafb2ce2a49b
Pradosh Priyadarshan <45397701+codepradosh@users.noreply.github.com>
2023-07-06 17:08:47 +0530

    Update xml.py

--- xml.py
+++ xml.py
@@ -31,3 +31,47 @@ with open(output_file, 'w', newline='') as csvfile:
 
             # Write the extracted fields to the CSV file
             writer.writerow(extracted_fields)
+
+
+
+
+
+
+import os
+import csv
+import xml.etree.ElementTree as ET
+
+folder_path = '/home/m809083/hosts'
+output_file = 'output.csv'
+fields = ['Host_alias', 'local', 'enabled', 'Origin', 'transport', 'Address', 'Port', 'user', 'readonly', 'Ftprootpath', 'Inbox', 'Outbox', 'Recieved', 'Sentbox']
+
+# Create the CSV file and write the header
+with open(output_file, 'w', newline='') as csvfile:
+    writer = csv.DictWriter(csvfile, fieldnames=fields)
+    writer.writeheader()
+
+    # Iterate through the XML files in the folder
+    for filename in os.listdir(folder_path):
+        if filename.endswith('.xml'):
+            file_path = os.path.join(folder_path, filename)
+
+            # Initialize a dictionary for the extracted fields
+            extracted_fields = {field: '' for field in fields}
+
+            # Parse the XML file
+            tree = ET.parse(file_path)
+            root = tree.getroot()
+
+            # Extract the desired fields from the XML file
+            extracted_fields['Host_alias'] = root.get('alias')
+            extracted_fields['local'] = root.get('local')
+            extracted_fields['enabled'] = root.get('enabled')
+
+            for field in fields[3:]:
+                element = root.find(field)
+                if element is not None:
+                    extracted_fields[field] = element.text
+
+            # Write the extracted fields to the CSV file
+            writer.writerow(extracted_fields)
+
