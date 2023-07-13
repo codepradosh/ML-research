b7ef26bc9578d9abeb8425ea00c39d3d48c10a74
Pradosh Priyadarshan <45397701+codepradosh@users.noreply.github.com>
2023-07-13 11:22:02 +0530

    Update ubs.py

--- ubs.py
+++ ubs.py
@@ -5,8 +5,9 @@ import xml.etree.ElementTree as ET
 folder_path = 'C:/Users/kanha930/Documents/Desktop/New folder'
 output_file = 'output.csv'
 
-# Define the fields to extract
+# Define the fields to extract and their desired order
 fields = ['Host_alias', 'local', 'enabled', 'Origin', 'transport', 'Address', 'Port', 'user', 'readonly', 'Ftprootpath', 'Inbox', 'Outbox', 'Recieved', 'Sentbox']
+field_order = fields + ['Aliases', 'Commands']
 
 # Create a list to store the extracted data
 data = []
@@ -25,42 +26,30 @@ for filename in os.listdir(folder_path):
         extracted_fields['Host_alias'] = root.get('alias')
         extracted_fields['local'] = root.get('local')
         extracted_fields['enabled'] = root.get('enabled')
-        extracted_fields['Origin'] = root.get('Origin', '')
-        extracted_fields['transport'] = root.get('transport', '')
-        extracted_fields['Address'] = root.find('Address').text.strip()
-        extracted_fields['Port'] = root.find('Port').text.strip()
-        extracted_fields['user'] = root.find('Username').text.strip()
-        extracted_fields['readonly'] = root.find('Readonly').text.strip()
-        extracted_fields['Ftprootpath'] = root.find('Ftprootpath').text.strip()
-        extracted_fields['Inbox'] = root.find('Inbox').text.strip()
-        extracted_fields['Outbox'] = root.find('Outbox').text.strip()
-        extracted_fields['Recieved'] = root.find('Receivedbox').text.strip()
-        extracted_fields['Sentbox'] = root.find('Sentbox').text.strip()
+
+        for child in root:
+            if child.tag in fields:
+                extracted_fields[child.tag] = child.text.strip()
 
         # Extract the aliases and commands under each "Action" tag
         actions = root.findall('.//Action')
-        for i, action in enumerate(actions):
-            aliases = []
-            commands_elem = action.find('Commands')
-            if commands_elem is not None:
-                commands = commands_elem.text.strip()
-            else:
-                commands = ''
+        aliases = []
+        commands = []
+        for action in actions:
             alias = action.get('alias')
             if alias is not None:
                 aliases.append(alias)
+            commands_elem = action.find('Commands')
+            if commands_elem is not None:
+                commands.append(commands_elem.text.strip())
 
-            extracted_fields[f'Action_Type_{i+1}'] = action.get('actiontype')
-            extracted_fields[f'Aliases_{i+1}'] = '\n'.join(aliases)
-            extracted_fields[f'Commands_{i+1}'] = commands
+        extracted_fields['Aliases'] = '\n'.join(aliases)
+        extracted_fields['Commands'] = '\n'.join(commands)
 
         data.append(extracted_fields)
 
-# Extract all unique column names
-column_names = set().union(*[d.keys() for d in data])
-
-# Write the extracted data to the CSV file
+# Write the extracted data to the CSV file with the desired field order
 with open(output_file, 'w', newline='', encoding='utf-8') as csvfile:
-    writer = csv.DictWriter(csvfile, fieldnames=column_names)
+    writer = csv.DictWriter(csvfile, fieldnames=field_order)
     writer.writeheader()
     writer.writerows(data)
