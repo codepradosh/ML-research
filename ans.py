c3ea07a566232e75c0028cc787ce9064b6f13e28
Pradosh Priyadarshan <45397701+codepradosh@users.noreply.github.com>
2023-07-24 22:00:24 +0530

    Update ans.py

--- ans.py
+++ ans.py
@@ -1,38 +1,61 @@
-import os
-import csv
-import xml.etree.ElementTree as ET
+app.agent(STRONG_TRAIN_TOPIC)
+#handles operations in async while consuming messages from kafka topic: Strong_TRAIN_TOPIC
+async def agent_weakpred_job(stream):
+    async for stream_item in stream.events():  #iterates over the kafka topic stream
 
-folder_path = '/home/m809083/hosts'
-output_file = 'output.csv'
+        all_partitions_processed = False
 
+        for partition in range(0, settings["num_partitions"]):
+            df_agg_pred = pd.DataFrame()
+            df_agg_trend = pd.DataFrame()
+            df_agg_season = pd.DataFrame()
+            df_agg_total = pd.DataFrame()
 
-ignore_nested_tags = True
+            clusterit = CLUSTERTABLEDB.iteritems()  #iterator to iterate over clustertabledatabse
+            modelit = WEAKMODELDB.iteritems() #iterator to iterate over weakmodel DB database
+            clusterit.seek_to_first() # indexes reset to first record
+            modelit.seek_to_first() # indexes reset to first record
 
-field_set = set()
-for filename in os.listdir(folder_path):
-    if filename.endswith('.xml'):
-        file_path = os.path.join(folder_path, filename)
-        tree = ET.parse(file_path)
-        root = tree.getroot()
-        for element in root.iter():
-            if ignore_nested_tags and any(child.tag == element.tag for child in element.iter()):
-                continue
-            field_set.add(element.tag)
+            #intialising dictionaries and lists
+            df_agg_pred = {}
+            df_agg_trend = {}
+            df_agg_season = {}
+            columnlist = {}
+            mname = {}
+            lstm_model = {}
+            ci_list = []
 
-fields = sorted(list(field_set))
-
-with open(output_file, 'w', newline='') as csvfile:
-    writer = csv.DictWriter(csvfile, fieldnames=fields)
-    writer.writeheader()
-
-    for filename in os.listdir(folder_path):
-        if filename.endswith('.xml'):
-            file_path = os.path.join(folder_path, filename)
-            extracted_fields = {field: '' for field in fields}
-            tree = ET.parse(file_path)
-            root = tree.getroot()
-            for element in root.iter():
-                if ignore_nested_tags and any(child.tag == element.tag for child in element.iter()):
+            # Processing Data from the Cluster Table
+            for each_key, each_val in clusterit:
+                if each_key == b'unique' or each_key == b'__faust\x00offset__':
                     continue
-                extracted_fields[element.tag] = element.text
-            writer.writerow(extracted_fields)
+
+                tseries = each_key.decode('UTF-8') #timeseries-name
+                model_name = str(each_val.decode().split(",")[0]) #model name(cluster name)
+                ci_list.append(tseries.split(".")[0]) # appends the cluster identifier by splitting the timeseries name
+                mname[tseries] = model_name #assosiates the time series with its corresponding model name(cluster_name)
+
+                #group time series under their respective clusters
+                if model_name in columnlist:
+                    columnlist[model_name.append(tseries)]
+                else:
+                    columnlist[model_name] = [tseries]
+
+            for key, val in columnlist.items():
+                #Creating DataFrames for Each Model
+                df_agg_pred[key] = pd.DataFrame()
+                df_agg_trend[key] = pd.DataFrame()
+                df_agg_season[key] = pd.DataFrame()
+
+                # Processing Data for Each Model
+                # The rest of your code for processing data for each model goes here
+
+                # Assuming you have some code here to process the data in each model
+
+            # Set the flag to True if this is the last partition to process
+            if partition == settings["num_partitions"] - 1:
+                all_partitions_processed = True
+
+            # Break the loop if all partitions have been processed
+            if all_partitions_processed:
+                break
