c71231b19e90e3fe772e37fa99a7cb00895aebf6
Pradosh Priyadarshan <45397701+codepradosh@users.noreply.github.com>
2023-09-28 15:35:11 +0530

    Update Democode.py

--- Democode.py
+++ Democode.py
@@ -1,119 +1,83 @@
-import pandas as pd
-import numpy as np
-from sklearn.ensemble import RandomForestClassifier
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
-from statsmodels.tsa.seasonal import STL
-from scipy.stats import skew, kurtosis
-from statsmodels.tsa.arima.model import ARIMA
-from sklearn.ensemble import ExtraTreesRegressor
+#!/bin/sh
+''''which python2 >/dev/null 2>&1 && exec python2 "$0" "$@" # '''
+''''which python3 >/dev/null 2>&1 && exec python3 "$0" "$@" # '''
+''''which python >/dev/null 2>&1 && exec python "$0" "$@" # '''
 
-# Read the dataset
-df = pd.read_csv("dummy_dataset.csv")
+#!/usr/bin/env python
 
+import subprocess
+import sys
 
-X = df.iloc[:, :-1]  
-y = df.iloc[:, -1]   
+def get_csb_version():
+    try:
+        result = subprocess.Popen(['csb', 'version'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        output, error_output = result.communicate()
+        output = output.strip()
+        error_output = error_output.strip()
+        
+        if result.returncode != 0:
+            print('Error getting CSB version: {}'.format(error_output))
+            sys.exit(1)
+        
+        # Extract the first digit of the CSB version (e.g., 3.x.x, 4.x.x, 8.x.x)
+        csb_version = int(output.split('.')[0])
+        return csb_version
+    except Exception as e:
+        print('Error getting CSB version: {}'.format(e))
+        sys.exit(1)
 
+def execute_service_command(service_name, action, csb_version):
+    if csb_version == 3:
+        # For CSB version 3, use 'dzdo service' commands
+        if action == 'start':
+            command = 'dzdo service {0} start'.format(service_name)
+        elif action == 'stop':
+            command = 'dzdo service {0} stop'.format(service_name)
+        elif action == 'status':
+            command = 'dzdo service {0} status'.format(service_name)
+        elif action == 'restart':
+            command = 'dzdo service {0} stop && dzdo service {0} start'.format(service_name)
+        else:
+            print('Invalid action. Use one of: start, stop, restart, status')
+            sys.exit(1)
+    elif csb_version in (4, 8):
+        # For CSB version 4 and 8, use 'dzdo systemctl' commands
+        if action == 'start':
+            command = 'dzdo systemctl start {0}'.format(service_name)
+        elif action == 'stop':
+            command = 'dzdo systemctl stop {0}'.format(service_name)
+        elif action == 'status':
+            command = 'dzdo systemctl status {0}'.format(service_name)
+        elif action == 'restart':
+            command = 'dzdo systemctl stop {0} && dzdo systemctl start {0}'.format(service_name)
+        else:
+            print('Invalid action. Use one of: start, stop, restart, status')
+            sys.exit(1)
+    else:
+        print('Unsupported CSB version.')
+        sys.exit(1)
 
-scaler = MinMaxScaler()
-X_normalized = scaler.fit_transform(X)
+    try:
+        result = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        output, error_output = result.communicate()
+        
+        if result.returncode != 0:
+            error_output = error_output.strip()
+            print('Error executing command: {}'.format(error_output))
+            sys.exit(1)
+        
+        print('Service "{0}" {1}ed successfully.'.format(service_name, action))
+    except Exception as e:
+        print('Error executing command: {}'.format(e))
+        sys.exit(1)
 
+if __name__ == '__main__':
+    if len(sys.argv) != 3:
+        print('Usage: ./DMZ.py <service_name> <action>')
+        sys.exit(1)
 
-lags = [1, 2, 3]  
-window_sizes = [3, 5, 7]  
-span_values = [3, 5, 7]  
-X_features = pd.DataFrame(X_normalized)
+    service_name = sys.argv[1]
+    action = sys.argv[2].lower()  # Action: start, stop, restart, status
+    csb_version = get_csb_version()
 
-
-for col in X.columns:
-    for lag in lags:
-        X_features[f"{col}_lag_{lag}"] = X[col].shift(lag)
-
-
-for col in X.columns:
-    for window_size in window_sizes:
-        X_features[f"{col}_mean_{window_size}"] = X[col].rolling(window=window_size).mean()
-        X_features[f"{col}_std_{window_size}"] = X[col].rolling(window=window_size).std()
-        X_features[f"{col}_min_{window_size}"] = X[col].rolling(window=window_size).min()
-        X_features[f"{col}_max_{window_size}"] = X[col].rolling(window=window_size).max()
-
-
-for col in X.columns:
-    for span in span_values:
-        X_features[f"{col}_ema_{span}"] = X[col].ewm(span=span).mean()
-
-
-for col in X.columns:
-    decomposition = STL(X[col]).fit()
-    X_features[f"{col}_trend"] = decomposition.trend
-    X_features[f"{col}_seasonal"] = decomposition.seasonal
-    X_features[f"{col}_residual"] = decomposition.resid
-
-
-for col in X.columns:
-    model = ExtraTreesRegressor(n_estimators=100)
-    model.fit(np.arange(len(X)).reshape(-1, 1), X[col])
-    X_features[f"{col}_trend"] = model.predict(np.arange(len(X)).reshape(-1, 1))
-
-
-for col in X.columns:
-    fft_vals = np.fft.fft(X[col])
-    X_features[f"{col}_fft_mean"] = np.mean(fft_vals)
-    X_features[f"{col}_fft_std"] = np.std(fft_vals)
-    X_features[f"{col}_fft_skew"] = skew(fft_vals)
-    X_features[f"{col}_fft_kurtosis"] = kurtosis(fft_vals)
-
-
-for col in X.columns:
-    X_features[f"{col}_skewness"] = skew(X[col])
-    X_features[f"{col}_kurtosis"] = kurtosis(X[col])
-
-
-for col1 in X.columns:
-    for col2 in X.columns:
-        if col1 != col2:
-            cross_corr = np.correlate(X[col1], X[col2])
-            X_features[f"{col1}_{col2}_cross_corr"] = cross_corr
-
-
-for col in X.columns:
-    X_features[f"{col}_entropy"] = -np.sum(X[col] * np.log2(X[col]))
-
-
-for col in X.columns:
-    model = ARIMA(X[col], order=(3, 0, 1))
-    model_fit = model.fit()
-    _, _, resid = model_fit.forecast(len(X))
-    X_features[f"{col}_forecast_residual"] = resid
-
-
-X_train, X_test, y_train, y_test = train_test_split(X_features, y, test_size=0.2, random_state=42)
-
-
-model = RandomForestClassifier()
-model.fit(X_train, y_train)
-
-
-importance_scores = model.feature_importances_
-
-
-feature_importance = dict(zip(X_features.columns, importance_scores))
-
-
-total_importance = sum(importance_scores)
-weights = {feature: score / total_importance for feature, score in feature_importance.items()}
-
-
-composite_scores = np.dot(X_features.values, list(weights.values()))
-
-
-threshold = 0.5
-
-
-predictions = np.where(composite_scores >= threshold, 1, 0)
-
-
-accuracy = accuracy_score(y, predictions)
-print("Accuracy:", accuracy)
+    execute_service_command(service_name, action, csb_version)
