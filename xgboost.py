ba1bc17106d4203e5d0351a6d99623161f0ef634
Pradosh Priyadarshan <45397701+codepradosh@users.noreply.github.com>
2023-10-03 23:31:46 +0530

    Update xgboost.py

--- xgboost.py
+++ xgboost.py
@@ -22,3 +22,7 @@ logger.addHandler(log_handler)
 def log_and_exit(message, exit_code=1):
     logger.error(message)
     sys.exit(exit_code)
+
+
+
+log_and_exit('Error getting CSB version: {}'.format(error_output))
