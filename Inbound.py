f661f1e326a5f764dfb9c42aeb4a8aec875b9f98
Pradosh Priyadarshan <45397701+codepradosh@users.noreply.github.com>
2023-10-05 16:56:27 +0530

    Update Inbound.py

--- Inbound.py
+++ Inbound.py
@@ -1,22 +1,79 @@
-# Decompose metrics into seasonality, trend, and residual using STL
-stl = STL(df['metrics'], seasonal=13)  # You can adjust the seasonal period as needed
-result = stl.fit()
-df['seasonality'] = result.seasonal
-df['trend'] = result.trend
-df['residual'] = result.resid
+df['timestamp'] = pd.to_datetime(df['timestamp'])
+df['timestamp'] = (df['timestamp'] - pd.Timestamp("1970-01-01")) // pd.Timedelta('1s')
 
-# Calculate simple moving averages (SMA) for each component
-window_size = 3  # Adjust the window size as needed
-components = ['metrics', 'seasonality', 'trend', 'residual']
-for component in components:
-    df[f'{component}_sma'] = df[component].rolling(window=window_size, min_periods=1).mean()
-
-# Calculate exponential moving averages (EMA) for each component
-alpha = 0.2  # Adjust the smoothing factor as needed
-for component in components:
-    df[f'{component}_ema'] = df[component].ewm(alpha=alpha, adjust=False).mean()
-
-# Normalize the features
+# Normalize the features (use the same scaler used during training)
 scaler = MinMaxScaler()
-feature_columns = ['metrics', 'seasonality', 'trend', 'residual', 'metrics_sma', 'seasonality_sma', 'trend_sma', 'residual_sma', 'metrics_ema', 'seasonality_ema', 'trend_ema', 'residual_ema']
+feature_columns = ['timestamp', 'metrics', 'seasonality', 'trend', 'residual', 'metrics_sma', 'seasonality_sma', 'trend_sma', 'residual_sma', 'metrics_ema', 'seasonality_ema', 'trend_ema', 'residual_ema']
 df[feature_columns] = scaler.fit_transform(df[feature_columns])
+
+# Prepare the data for prediction
+sequence_length = 10  # Adjust the sequence length as needed
+num_features = len(feature_columns)
+X = []
+
+for i in range(len(df) - sequence_length):
+    X.append(df[feature_columns].values[i:i + sequence_length])
+
+X = np.array(X)
+
+# Split the data into training and validation sets
+split_ratio = 0.8
+split_index = int(split_ratio * len(X))
+
+X_train, X_val = X[:split_index], X[split_index:]
+y_train, y_val = y[:split_index], y[split_index:]
+
+# Define hyperparameters for model tuning
+lstm_units = 128  # Number of LSTM units in the hidden layer
+learning_rate = 0.001
+batch_size = 32
+epochs = 50
+
+# Build the LSTM model
+model = Sequential([
+    LSTM(units=lstm_units, input_shape=(sequence_length, num_features), activation='relu'),
+    Dense(1)
+])
+
+# Compile the model with the chosen optimizer, learning rate, and loss function
+optimizer = Adam(learning_rate=learning_rate)
+model.compile(optimizer=optimizer, loss='mse')
+
+# Define early stopping callback
+early_stopping = EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
+
+# Train the model
+history = model.fit(
+    X_train, y_train,
+    validation_data=(X_val, y_val),
+    epochs=epochs,
+    batch_size=batch_size,
+    callbacks=[early_stopping]
+)
+
+# Use the trained model to predict residuals
+predicted_residuals = model.predict(X_val)
+
+# Calculate Z-scores for the residuals using statistics from the training data
+mean_residual = np.mean(y_train)  # Use the mean and std from training data
+std_residual = np.std(y_train)
+z_scores = (predicted_residuals - mean_residual) / std_residual
+
+# Set a 3-sigma threshold for anomaly detection
+threshold = 3
+
+# Detect anomalies based on Z-scores and threshold
+anomalies = np.abs(z_scores) > threshold
+
+# Print timestamps where anomalies are detected
+anomaly_timestamps = df.iloc[split_index + sequence_length:][anomalies]['timestamp']
+print("Anomaly Timestamps:")
+print(anomaly_timestamps)
+
+# Plot training and validation loss
+plt.plot(history.history['loss'], label='Training Loss')
+plt.plot(history.history['val_loss'], label='Validation Loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
